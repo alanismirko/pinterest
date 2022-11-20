c2f23b793f33bdfe7ee931c8db48aeb89213bc21
alanismirko <alanismirko@gmail.com>
2022-11-20 21:56:29 +0100

    reorganizing files | adding initialize.php

---
+++ private/initialize.php
@@ -0,0 +1,16 @@
+<?php
+
+  // session_start(); // turn on sessions if needed
+
+  // Assign file paths to PHP constants
+  // __FILE__ returns the current path to this file
+  // dirname() returns the path to the parent directory
+  define("PRIVATE_PATH", dirname(__FILE__));
+  define("PROJECT_PATH", dirname(PRIVATE_PATH));
+  define("PUBLIC_PATH", PROJECT_PATH . '/public');
+  define("STATIC_PATH", PROJECT_PATH . '/static');
+  define("SHARED_PATH", STATIC_PATH . '/shared');
+
+
+
+?>
