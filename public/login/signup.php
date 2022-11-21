1d9f4b31ab1540f6fdc7334460dcfe950dc08342
alanismirko <alanismirko@gmail.com>
2022-11-21 21:34:21 +0100

    initial for login

--- public/login/signup.php
+++ public/login/signup.php
@@ -2,3 +2,13 @@
 
 <?php include(SHARED_PATH . '/header.php'); ?>
 
+<div id="login" class="column-wrapper">
+    <h1>Sign up to get your ideas</h1>
+
+    <div class="info-wrapper">
+
+    </div>
+</div>
+
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
