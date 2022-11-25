9f2a97c85777ff010e5b4e9e4582be836b5877f2
alanismirko <alanismirko@gmail.com>
2022-11-25 14:20:38 +0100

    card design

--- public/pinterest/views/index.php
+++ public/pinterest/views/index.php
@@ -3,6 +3,27 @@
 <?php require_once(SHARED_PATH . '/header_pinterest.php'); ?>
 
 
-
+<div class="image-list-wrapper">
+    <div class="image-container">
+        <div class="card">
+            <img src="../../static/assets/list/image1.svg" alt="">
+        </div>
+        <div class="card">
+            <img src="../../static/assets/list/image2.svg" alt="">
+        </div>
+        <div class="card">
+            <img src="../../static/assets/list/image3.svg" alt="">
+        </div>
+        <div class="card">
+            <img src="../../static/assets/list/image4.svg" alt="">
+        </div>
+        <div class="card">
+            <img src="../../static/assets/list/image5.svg" alt="">
+        </div>
+        <div class="card">
+            <img src="../../static/assets/list/image6.svg" alt="">
+        </div>
+    </div>
+</div>
 
 <?php include(SHARED_PATH . '/footer.php'); ?>
--- public/static/shared/header_pinterest.php
+++ public/static/shared/header_pinterest.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../static/style/css/style.css">
+    <link rel="icon" href="https://s.pinimg.com/webapp/favicon-54a5b2af.png">
+
     <title>Pinterest</title>
 </head>
 <body>
@@ -17,9 +19,15 @@
     </form>
 
     <div class="dropdown-wrapper">
-    <img class="round" src="../../static/assets/create.svg" alt="">
+    <a href="#">
+        <img class="round" src="../../static/assets/create.svg" alt="">
+    </a>
+
+    <button>
     <img src="../../static/assets/dropwdown.svg" alt="">
+
+    </button>
     </div>
-   
+
 </nav>
     
