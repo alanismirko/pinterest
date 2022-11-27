341e964303122bd203963d4d2417249fe5682b04
alanismirko <alanismirko@gmail.com>
2022-11-27 20:53:17 +0100

    logout

--- private/logout.php
+++ private/logout.php
@@ -6,16 +6,22 @@ require_once('db.php');
 try{
 
     if (isset($_COOKIE['login'])) {
-        $session_id = $_COOKIE["login"];
-        $query_user = $db->prepare('DELETE  FROM sessions WHERE session_id=:session_id');
-        $query_user->bindValue(':session_id', $session_id);
-        $query_user->execute();
+        $session_id = $_COOKIE['login'];
+        print_r($session_id);
+        flush();
+        try{
+            $query_user = $db->prepare('DELETE FROM sessions WHERE session_id=:session_id');
+            $query_user->bindValue(':session_id', $session_id);
+            $query_user->execute();
+        }catch(PDOException $ex){
+            echo $ex;
+        }
         unset($_COOKIE['login']); 
         header("location: ./login.php"); 
     } else {
         header('Location: ../public/pinterest/views/index.php');
     }
     
-}catch(PDOException $ex){
+}catch(Exception $ex){
     echo $ex;
 }
