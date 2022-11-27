37623aed11c323b8f2f622e898865e47e4ed6bac
alanismirko <alanismirko@gmail.com>
2022-11-27 21:48:58 +0100

    navbar dropdown

--- public/static/shared/header_pinterest.php
+++ public/static/shared/header_pinterest.php
@@ -29,8 +29,26 @@
         <div id="dropdownButton">
             <img  src="../../static/assets/dropwdown.svg" alt="">
             <div id="dropdownModal">
-                <a href="">Something</a>
-                <a href="">Something 2</a>
+
+                <p>Currently in</p>
+                <div class="profile-link-wrapper">
+                    <p class="userLetter">I</p>
+                    <div class="user-info-wrapper">
+                        <p><strong>Username</strong></p>
+                        <p>Personal</p>
+                        <p>email</p>
+                    </div>
+                </div>
+
+                <p>Your accounts</p>
+                <a href="#">Add account</a>
+                <a href="#">Convert to business</a>
+
+                <p>More options</p>
+                <a href="#">Settings</a>
+                <a href="https://help.pinterest.com/en?source=gear_menu_web">Get help</a>
+                <a href="https://policy.pinterest.com/en/terms-of-service">See terms of service</a>
+                <a href="https://policy.pinterest.com/en/privacy-policy">See privacy policy</a>
                 <a href="../../../private/logout.php">Logout</a>
             </div>
         </div>
