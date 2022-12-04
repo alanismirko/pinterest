b020be752c0695555095327bbd373fb70739782c
alanismirko <alanismirko@gmail.com>
2022-12-04 21:42:57 +0100

    deleting user

--- public/admin/apis/delete_user.php
+++ public/admin/apis/delete_user.php
@@ -7,12 +7,12 @@ error_reporting(E_ALL);
 include_once('./././private/admin/users.php');
 
 $users = new Users();
-echo $_GET['user_id'];
+$url_id = explode("/",($_SERVER["REQUEST_URI"])); 
 
-if(!isset($_GET['user_id']) ){
-    $user_id= $_GET['user_id'];
-    $users->delete_user($user_id);
+$user_id= $url_id[3];
+$users->delete_user($user_id);
 
-}
+header('Location:/admin');
+
 
 
